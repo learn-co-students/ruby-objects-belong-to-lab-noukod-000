266554193f6a208b97a7752b7ad4c35f12b03b13
Samuel Cornet <samuel@noukod.com>
2018-10-03 15:42:41 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,8 @@
+class Post
+
+      attr_accessor :title, :author
+
+      def initialize
+        self.author = author
+      end
+end
---
+++ lib/song.rb
@@ -0,0 +1,8 @@
+class Song
+
+      attr_accessor :title, :artist
+
+      def initialize
+        self.artist = artist
+      end
+end
